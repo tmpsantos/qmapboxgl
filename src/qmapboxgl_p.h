9dbc3499ffe79aad16cb5d583a895ab045771814
Bruno de Oliveira Abinader <brunoabinader@gmail.com>
2015-06-03 15:51:46 +0300

    Use final for virtual methods on private impl of mbgl::View
    This is a private implementation, so we know that no one else will
    inherit from that.

--- src/qmapboxgl_p.h
+++ src/qmapboxgl_p.h
@@ -21,10 +21,10 @@ public:
     virtual ~QMapboxGLPrivate();
 
     // mbgl::View implementation.
-    void activate() override {}
-    void deactivate() override;
-    void notify() override {}
-    void invalidate(std::function<void()> renderCallback) override;
+    void activate() final {}
+    void deactivate() final;
+    void notify() final {}
+    void invalidate(std::function<void()> renderCallback) final;
 
     mbgl::DefaultFileSource fileSource;
     mbgl::Map map;
